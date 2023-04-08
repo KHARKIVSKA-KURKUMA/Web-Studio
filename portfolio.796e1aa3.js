562e9dfdca9d26dc3b055da9429b2627222497bd
KHARKIVSKA-KURKUMA <99280393+KHARKIVSKA-KURKUMA@users.noreply.github.com>
2023-04-08 14:07:19 +0000

    Deploying to gh-pages from @ KHARKIVSKA-KURKUMA/Web-Studio@2d2ce1fae7cfb6c933c8638b2f50180b761db533 🚀

---
+++ portfolio.796e1aa3.js
@@ -0,0 +1,2 @@
+var upBtn=document.getElementById("up");function scrollFunction(){document.body.scrollTop>500||document.documentElement.scrollTop>500?(upBtn.style.display="flex",upBtn.classList.remove("is-hidden")):upBtn.style.display="none"}function topFunction(){document.body.scrollTop=0,document.documentElement.scrollTop=0}window.onscroll=function(){scrollFunction()},upBtn.addEventListener("click",topFunction);
+//# sourceMappingURL=portfolio.796e1aa3.js.map
