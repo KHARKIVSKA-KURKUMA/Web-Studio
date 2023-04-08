2d2ce1fae7cfb6c933c8638b2f50180b761db533
Vasiuk Kateryna <catherine.vasyuk11@gmail.com>
2023-04-08 17:06:51 +0300

    add up btn

---
+++ src/js/up-btn.js
@@ -0,0 +1,18 @@
+const upBtn = document.getElementById('up');
+window.onscroll = function () {
+  scrollFunction();
+};
+upBtn.addEventListener('click', topFunction);
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    upBtn.style.display = 'flex';
+    upBtn.classList.remove('is-hidden');
+  } else {
+    upBtn.style.display = 'none';
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
